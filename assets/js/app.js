dd89930d618d388985a8958626d9ddaac558eadb
MaJu2023 <142547953+MaJu2023@users.noreply.github.com>
2023-09-05 08:15:33 -0600

    Update app.js
    Agregar comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,12 @@ let change = document.querySelector(".change-color");
 let colorName = document.querySelector(".color");
 
 function randomColor(){
-
+    
+//Generar tres valores aleatorios en formato hexadecimal para representar un color RGB (rojo, verde, azul), por ejemplo "F1A2B3".
+//Generar un número decimal aleatorio entre 0 y 1 y multiplicarlo por 256 y redondearlo hacia abajo para generar un numero aleatorio entre 0 y 256.
+//Convertir el número a una cadena en formato hexadecimal
+//Asegurar que la cadena tenga al menos dos caracteres, si solo tiene un caracter se agrega un 0 al principio.
+    
     const r = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
     const g = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
     const b = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
